a8ae1e71c1d3b2c76202e3159178298c1b73176e
lev <levdanilin@gmail.com>
2024-02-05 17:37:09 +0100

    Added StockEndpoint

--- src/Controller/DatabaseController.php
+++ src/Controller/DatabaseController.php
@@ -6,6 +6,7 @@ use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DatabaseController extends AbstractController
@@ -16,5 +17,26 @@ class DatabaseController extends AbstractController
         $productRepository = $entityManager->getRepository(Product::class);
         $products = $productRepository->getGeneralData();
         return new JsonResponse($products);
+    }
+
+    #[Route('api/stock', name: 'api_stock', methods: ['POST'])]
+    public function getStatus (EntityManagerInterface $entityManager): JsonResponse
+    {
+        $request = Request::createFromGlobals();
+        $productId = $request->get('product_id');
+        $quantity = (int)$request->get('quantity');
+
+        $productRepository = $entityManager->getRepository(Product::class);
+
+        /** @var Product $product */
+        $product = $productRepository->findOneBy(['product_id' => $productId]);
+        $message = 'Please provide necessary product data';
+
+        if($product)
+        {
+            $amount = $product->getStockAvailable();
+            $message = $amount !== 0 && $amount >= $quantity ? 'article can be fulfilled' : 'article can not be fulfilled';
+        }
+        return new JsonResponse($message);
     }
 }
