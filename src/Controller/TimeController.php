8bacef3b90df2ce79a2df4bbd6712ba36014c63a
lev <levdanilin@gmail.com>
2024-02-05 13:52:04 +0100

    Added TimeApiEndpoint

---
+++ src/Controller/TimeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+class TimeController extends AbstractController
+{
+    #[Route('api/time')]
+    public function getTime(): JsonResponse
+    {
+        $time = (new \DateTime())->format("Y-m-d H:i:s");
+        return new JsonResponse($time);
+    }
+}
